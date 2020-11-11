8de8a18904ac6463105c0b6ae4239061b6c87625
PaulPeriod <=>
2020-11-11 15:36:41 +0100

    Added baseline for Entity Framework, generic DbController and abstract DbModel

---
+++ Model/Startup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Model.Data;
+
+namespace Model
+{
+    public class Startup
+    {
+
+    }
+}
