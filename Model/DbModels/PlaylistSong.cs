4236ee53dcd609040f70ccd320258d6cfc71b141
Sander Pol <sander__pol@outlook.com>
2020-11-12 10:26:32 +0100

    Added Model for Playlist and Song

---
+++ Model/DbModels/PlaylistSong.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.DbModels
+{
+    public class PlaylistSong
+    {
+        public int SongID { get; set; }
+        public Song Song { get; set; }
+
+        public int PlaylistID { get; set; }
+        public Playlist Playlist { get; set; }
+    }
+}
---
+++ Model/DbModels/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Model.DbModels
+{
+    public class Song : DbModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Path { get; set; }
+
+        [Required]
+        public double Duration { get; set; }
+
+        public IList<PlaylistSong> PlaylistSongs { get; set; }
+    }
+}
