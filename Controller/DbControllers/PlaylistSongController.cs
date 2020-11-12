30d2de1be26911fd5570c253630784eaf82d1e64
Hai <wat.te.hai@gmail.com>
2020-11-12 13:04:35 +0100

    PlaylistSongController to be fixed

---
+++ Controller/DbControllers/PlaylistSongController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Model.Data;
+using Model.DbModels;
+
+namespace Controller.DbControllers
+{
+    public class PlaylistSongController : DbController<PlaylistSong>
+    {
+        public PlaylistSongController(DatabaseContext context, DbSet<PlaylistSong> set) : base(context, set)
+        {
+
+        }
+    }
+}
