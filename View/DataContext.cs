75eb616925e6e78d1cd63479d88fbdb557ab8c58
Sietse Bijlsma <s1142382@student.windesheim.nl>
2020-11-12 14:34:14 +0100

    FileTransfer

--- View/DataContext.cs
+++ View/DataContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Management.Automation.Language;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Timers;
@@ -11,9 +12,6 @@ namespace View
     public class DataContext : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
-
-        public double TotalTime => Data.CurrentSong.TotalTimeSong;
-        public double CurrentTime => Data.CurrentSong.CurrentTimeSong;
 
         public string TotalTimeLabel => Math.Floor(Data.CurrentSong.TotalTimeSong / 60) + ":" + string.Format("{0:00}",Math.Floor(Data.CurrentSong.TotalTimeSong % 60));
         public string CurrentTimeLabel => Math.Floor(Data.CurrentSong.CurrentTimeSong / 60) + ":" + string.Format("{0:00}",Math.Floor(Data.CurrentSong.CurrentTimeSong % 60));
