cc3d895f7f00064763c9057f961a113f9d330c93
Hai <wat.te.hai@gmail.com>
2020-11-12 12:03:59 +0100

    Added Playlist- and SongController

---
+++ Controller/DbControllers/PlaylistController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Model.Data;
+using Model.DbModels;
+
+namespace Controller.DbControllers
+{
+    public class PlaylistController: DbController<Playlist>
+    {
+        public PlaylistController(DatabaseContext context, DbSet<Playlist> set) : base(context, set)
+        {
+        }
+    }
+}
---
+++ Controller/DbControllers/SongController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Model.Data;
+using Model.DbModels;
+
+namespace Controller.DbControllers
+{
+    public class SongController: DbController<Song>
+    {
+        public SongController(DatabaseContext context, DbSet<Song> set) : base(context, set)
+        {
+        }
+    }
+}
