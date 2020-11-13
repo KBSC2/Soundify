22b15deeee52a07d5caabdb4db3dbb2e99b90dd6
Rblindeboom <57721695+Rblindeboom@users.noreply.github.com>
2020-11-13 11:53:02 +0100

    Update PlaylistController.cs
    Playlist adden

--- Controller/DbControllers/PlaylistController.cs
+++ Controller/DbControllers/PlaylistController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
@@ -9,8 +9,25 @@ namespace Controller.DbControllers
 {
     public class PlaylistController: DbController<Playlist>
     {
-        public PlaylistController(DatabaseContext context, DbSet<Playlist> set) : base(context, set)
+        private DatabaseContext _context;
+        private PlaylistController _playlistController;
+        public PlaylistController(DatabaseContext context, DbSet<Playlist> playlist) : base(context, playlist)
         {
+            this._context = context;
+            this._playlistController = new PlaylistController(context, playlist);
+        }
+        public void AddPlaylist(int playlistID)
+        {
+            var playlist = new Playlist()
+            {
+                PlaylistID = playlistID
+
+            };
+            _context.Playlists.Add(playlist);
+            _context.Entry(playlist).State = EntityState.Added;
+            _context.SaveChanges();
+
+
         }
     }
 }
