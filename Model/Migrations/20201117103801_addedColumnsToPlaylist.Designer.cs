6a32ca043acff20cac2f72a9231230da0fd7ea72
Sander Pol <sander__pol@outlook.com>
2020-11-17 11:39:35 +0100

    Added Migration

--- Model/Migrations/20201117103801_addedColumnsToPlaylist.Designer.cs
+++ Model/Migrations/20201117103801_addedColumnsToPlaylist.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Model.Data;
 
 namespace Model.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20201117103801_addedColumnsToPlaylist")]
+    partial class addedColumnsToPlaylist
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Model/Migrations/20201117103801_addedColumnsToPlaylist.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Model.Migrations
+{
+    public partial class addedColumnsToPlaylist : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ActivePlaylist",
+                table: "Playlists",
+                type: "bit",
+                nullable: false,
+                defaultValue: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeleteDateTime",
+                table: "Playlists",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ActivePlaylist",
+                table: "Playlists");
+
+            migrationBuilder.DropColumn(
+                name: "DeleteDateTime",
+                table: "Playlists");
+        }
+    }
+}
