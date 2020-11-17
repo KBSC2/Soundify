fcab3961f292441c50b07d0bd2ced87f92196817
Sander Pol <sander__pol@outlook.com>
2020-11-17 12:10:04 +0100

    AddedTestsforDeactivate-DeletePlaylist

---
+++ Tests/ControllorDeactivate-DeletePlaylist.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Controller.DbControllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.Data;
+using Model.DbModels;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ControllorDeactivate_DeletePlaylist
+    {
+        private DatabaseContext context;
+        private PlaylistController playlistController;
+        private Playlist playlist;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            context = new DatabaseContext();
+            playlistController = new PlaylistController(context, context.Playlists);
+            playlist = new Playlist(){Name = "TestDeactivateDelete", ActivePlaylist = true, CreationDate = DateTime.Now};
+
+        }
+        [Test]
+        public void DeactivatePlaylistTest()
+        {
+            playlistController.CreateItem(playlist);
+            playlistController.deactivatePlaylist(playlist.ID);
+            var testVariable = playlistController.GetItem(playlist.ID);
+            Assert.IsFalse(testVariable.ActivePlaylist);
+            playlistController.DeleteItem(playlist.ID);
+            
+        }
+
+        [Test, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void DeletePlaylistTest()
+        {
+            playlistController.CreateItem(playlist);
+            playlistController.deactivatePlaylist(playlist.ID);
+            var testVariable = playlistController.GetItem(playlist.ID);
+            testVariable.DeleteDateTime = DateTime.Now.AddMilliseconds(1500);
+            playlistController.UpdateItem(testVariable);
+            playlistController.deletePlaylistOnDateStamp();
+            Thread.Sleep(3000);
+            playlistController.GetItem(playlist.ID);
+        }
+
+       
+    }
+}
