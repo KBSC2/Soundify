ce252a51d0aa9d489dd745e25da489058c8da842
VincentVinTin <59730036+VincentVinTin@users.noreply.github.com>
2020-11-18 14:01:41 +0100

    codebehind file added

---
+++ View/Screens/PlaylistMenuScreen.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace View.Screens
+{
+    partial class PlaylistMenuScreen : ResourceDictionary
+    {
+        public PlaylistMenuScreen()
+        {
+            this.InitializeComponent();
+        }
+
+        private void test(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
