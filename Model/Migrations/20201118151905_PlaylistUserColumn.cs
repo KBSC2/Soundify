161f429fe7f09236d97917bb7e9f78b7f83748bb
PaulPeriod <=>
2020-11-18 16:31:04 +0100

    User Model and user table created

---
+++ Model/Migrations/20201118151905_PlaylistUserColumn.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Model.Migrations
+{
+    public partial class PlaylistUserColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserID",
+                table: "Playlists",
+                type: "int",
+                nullable: false,
+                defaultValue: true);
+        }
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserID",
+                table: "Playlists");
+        }
+    }
+}
