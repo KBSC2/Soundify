6b734debdf7e15d49e963e3a20f18aa97f7a7dc9
PaulPeriod <=>
2020-11-18 16:31:04 +0100

    User Model and user table created

---
+++ Model/DbModels/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Model.DbModels
+{
+    public class User : DbModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        [ForeignKey("UserID")]
+        public ICollection<Playlist> Playlists { get; set; }
+    }
+}
