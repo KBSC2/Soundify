2bf7a29de6a57c00e99f4a74bf3eafca4abcd5d3
Paul Rill <=>
2020-11-19 12:05:19 +0100

    Temp register screen connect

--- Model/DbModels/User.cs
+++ Model/DbModels/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
@@ -7,6 +8,9 @@ namespace Model.DbModels
 {
     public class User : DbModel
     {
+        [Required]
+        public string Username { get; set; }
+        [Required]
         public string Email { get; set; }
         public string Password { get; set; }
     }
