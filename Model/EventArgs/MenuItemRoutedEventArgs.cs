829fa14c7f85fc1451ecd18775af44824fc2af74
Paul Rill <=>
2020-11-19 12:53:34 +0100

    Reimported Proj-4 to branch

--- Model/EventArgs/MenuItemRoutedEventArgs.cs
+++ Model/EventArgs/MenuItemRoutedEventArgs.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
+using Model.DbModels;
 
 namespace Model.EventArgs
 {
     public class MenuItemRoutedEventArgs : RoutedEventArgs
     {
         public ScreenNames ScreenName { get; set; }
+        public Playlist Playlist { get; set; }
     }
 }
