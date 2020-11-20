516dbf477c021c474166a9fd20d52b28e85c3d91
Sietse Bijlsma <s1142382@student.windesheim.nl>
2020-11-20 11:06:32 +0100

    settingsscreen added

---
+++ View/Screens/SettingScreen.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using Soundify;
+
+namespace View.Screens
+{
+    partial class SettingScreen : ResourceDictionary
+    {
+        public SettingScreen()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Logout_Button_Click(object sender, RoutedEventArgs e)
+        {
+            new MainWindow().Hide();
+            DataContext.Instance.CurrentUser = null;
+            var login = new LoginScreen();
+            login.Show();
+            login.Focus();
+        }
+
+        private void ChangePassword_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
