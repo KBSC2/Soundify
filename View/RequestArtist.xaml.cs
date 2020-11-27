42af67ab6be5533d3e5d2c2a4ce082dbc39c24d6
WhatTheHai <haiboy@hotmail.nl>
2020-11-27 10:51:48 +0100

    soundify icons added, request artist screen

---
+++ View/RequestArtist.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace View
+{
+    /// <summary>
+    /// Interaction logic for RequestArtist.xaml
+    /// </summary>
+    public partial class RequestArtist : Window
+    {
+        public RequestArtist()
+        {
+            InitializeComponent();
+        }
+        private void Confirm_On_Enter_Key(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                Confirm_Button_Click(sender, new RoutedEventArgs());
+            }
+        }
+
+        private void Confirm_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Cancel_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+}
