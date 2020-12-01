1f89218133eaf1f912c1175c78e6158a027fb6bf
Hai <haiboy@hotmail.nl>
2020-12-01 12:14:58 +0100

    Kleine oepsie en test setup

---
+++ Tests/Requests/Controller_RequestController_AddRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Controller.DbControllers;
+using Model.Database.Contexts;
+using Model.DbModels;
+using NUnit.Framework;
+
+namespace Tests.Requests
+{
+    [TestFixture]
+    public class Controller_RequestController_AddRequest
+    {
+        private MockDatabaseContext _context;
+        private RequestController _requestController;
+        private Request _testRequest;
+        [SetUp]
+        public void SetUp()
+        {
+            _context = new MockDatabaseContext();
+            _requestController = new RequestController(_context);
+            _testRequest = new Request()
+                {UserID = 1, ArtistName = "TestArtist", ArtistReason = "Just because I'm testing"};
+
+        }
+    }
+}
