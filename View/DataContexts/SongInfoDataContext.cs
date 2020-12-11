bc01526e16787fafa64d6b9957d1bf2685a468bf
Sander Pol <sander__pol@outlook.com>
2020-12-11 15:16:10 +0100

    Auto stash before merge of "BUG-SongInfoInBottomMainWindow" and "origin/PROJ-58-only-show-songs-from-current-artist"

--- View/DataContexts/SongInfoDataContext.cs
+++ View/DataContexts/SongInfoDataContext.cs
@@ -7,7 +7,7 @@ namespace View.DataContexts
     class SongInfoDataContext : INotifyPropertyChanged
     {
         public Song Song { get; set; }
-        public string PathToImage => Song == null ? "" : Song.PathToImage == null ? "": FileCache.Instance.GetFile(Song.PathToImage); 
+        public string PathToImage => Song == null ? "../Assets/null.png" : Song.PathToImage == null ? "": FileCache.Instance.GetFile(Song.PathToImage); 
 
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged()
