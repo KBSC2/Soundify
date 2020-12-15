59f248d70f8a9831ae7c65aef1110b8a72b9e68c
Sietse Bijlsma <s1142382@student.windesheim.nl>
2020-12-15 16:33:35 +0100

    comments deleted

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -109,26 +109,6 @@ namespace Soundify
                         yield return childChild;
                 }
             }
-
-
-
-            /*if (depObj != null)
-            {
-                foreach (object rawChild in LogicalTreeHelper.GetChildren(depObj))
-                {
-                    if (rawChild is DependencyObject)
-                    {
-                        DependencyObject child = (DependencyObject) rawChild;
-                        if (child is T)
-                        {
-                            yield return (T) child;
-                        }
-
-                        foreach (T childOfChild in FindLogicalChildren<T>(child))
-                            yield return childOfChild;
-                    }
-                }
-            }*/
         }
 
         /**
