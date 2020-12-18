e340d254183f015b7adf61b2b4273867be977ef7
WhatTheHai <haiboy@hotmail.nl>
2020-12-18 13:47:13 +0100

    Home screen skeleton

---
+++ View/DataContexts/HomeDataContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Model.Annotations;
+
+namespace View.DataContexts
+{
+    public class HomeDataContext : INotifyPropertyChanged
+    {
+        private static HomeDataContext instance;
+        public static HomeDataContext Instance => instance ??= new HomeDataContext();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
