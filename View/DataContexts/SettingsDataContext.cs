95a0a5d310f1a2da37a4fa5e8c9becb59ae0645f
VincentVinTin <59730036+VincentVinTin@users.noreply.github.com>
2021-01-04 11:20:27 +0100

    Update SettingsDataContext.cs

--- View/DataContexts/SettingsDataContext.cs
+++ View/DataContexts/SettingsDataContext.cs
@@ -13,7 +13,7 @@ namespace View.DataContexts
         public static SettingsDataContext Instance => instance ??= new SettingsDataContext();
         public Role Role => UserController.CurrentUser.Role;
 
-        public int VolumeComboBoxSelectedIndex => AudioPlayer.Instance.MaxVolume == 0.1 ? 0 : AudioPlayer.Instance.MaxVolume == 0.4 ? 2 : 1;
+        public int VolumeComboBoxSelectedIndex => AudioPlayer.Instance.MaxVolume == 0.1 ? 0 : (AudioPlayer.Instance.MaxVolume == 0.4 ? 2 : 1);
 
         public string CurrentUserName => UserController.CurrentUser.Username;
 
